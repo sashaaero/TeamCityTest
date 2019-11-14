82c36eb0e8e8b2a5971d84432f1240f378b6eec3
Alexander Tischenko <sashaaero@yandex.ru>
2019-11-14 11:09:19 +0300

    Scripts and Tests

---
+++ scripts/foo.py
@@ -0,0 +1,23 @@
+from __future__ import division
+
+
+def add(a, b):
+    return a + b
+
+
+def sub(a, b):
+    return a - b
+
+
+def mul(a, b):
+    return a * b
+
+
+def div(a, b):
+    if b == 0:
+        raise ZeroDivisionError('You cannot divide by 0')
+    return a / b
+
+
+def pow(a, b):
+    return a ** b
---
+++ tests/test_scripts.py
@@ -0,0 +1,24 @@
+import unittest
+from scripts.foo import *
+
+
+class Test(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(add(5, 10), 15)
+
+    def test_sub(self):
+        self.assertEqual(sub(10, 5), 5)
+
+    def test_mul(self):
+        self.assertEqual(mul(5, 10), 50)
+
+    def test_div(self):
+        self.assertEqual(div(10, 5), 2)
+
+    def test_div_2(self):
+        with self.assertRaises(ZeroDivisionError) as context:
+            div(10, 0)
+        self.assertTrue('You cannot divide by 0' in str(context.exception))
+
+    def test_pow(self):
+        self.assertTrue(pow(10, 2), 100)
