7e3f78556d2ab4bc39e6d6e246827a4f4bdc60d9
Alexander Tischenko <sashaaero@yandex.ru>
2020-01-10 18:33:27 +0300

    Sabotage

--- tests/test_scripts.py
+++ tests/test_scripts.py
@@ -3,15 +3,15 @@ from scripts.foo import *
 from utils import get_test_runner
 
 
-class Test(unittest.TestCase):
+class TestScripts(unittest.TestCase):
     def test_add(self):
-        self.assertEqual(add(5, 10), 15)
+        self.assertEqual(15, add(10, 5))
 
     def test_sub(self):
-        self.assertEqual(sub(10, 5), 5)
+        self.assertEqual(5, sub(10, 5))
 
     def test_mul(self):
-        self.assertEqual(mul(5, 10), 50)
+        self.assertEqual(50, mul(5, 10))
 
     def test_div(self):
         self.assertEqual(div(10, 5), 2)
